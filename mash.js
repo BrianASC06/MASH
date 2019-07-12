9fc453392e38fc6251a9cb30a8a855075d0eb9ef
BrianASC06 <brianramirez881@gmail.com>
2019-07-12 14:38:02 -0400

    MASH end of week project

---
+++ mash.js
@@ -0,0 +1,35 @@
+
+
+function gethome(){
+    let favorite = process.argv[2];
+    if(favorite == undefined){
+        return 'kennel';
+    }
+    let Homes = ['Mansion', 'Apartment', 'Shack', 'House', favorite,'Street'];
+    return Homes[Math.floor(Math.random()*Homes.length)];   
+}
+function getChildrenCount(){    
+    let randomnumber = process.argv[3];
+if(randomnumber == undefined){
+    return 'infinite';
+};
+if(Math.random() > 0.5){
+    return Math.floor(Math.random() * 100);
+}else{
+    return randomnumber;
+}    
+    
+};
+getChildrenCount();
+function getcar(){
+    let car = process.argv[4];
+if(car == undefined){
+    return 'air';
+}
+    let cars = ['Lambo', 'convertible', 'box with wheels', 'nothing', car,'toy car'];
+    return cars[Math.floor(Math.random()*cars.length)];   
+}
+function mash(){
+    return 'You will live in a ' + gethome() +', and you will have '+ getChildrenCount() + ' kids! You will also have a ' + getcar()+ '!';
+}
+console.log(mash());
